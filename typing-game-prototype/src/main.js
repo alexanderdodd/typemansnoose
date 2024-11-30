b72aa3259226266eaa63fa397b41feec78090ab1
arran.toomer <arran.toomer@costcutter.com>
2024-11-30 17:17:58 +0000

    progress

--- typing-game-prototype/src/main.js
+++ typing-game-prototype/src/main.js
@@ -35,3 +35,5 @@ document.getElementById("textInput").addEventListener("keyup", (val) => {
   document.getElementById("textOutput").innerHTML =
     document.getElementById("textOutput").innerHTML + val.key;
 });
+
+document.getElementById("wordsDisplay").innerHTML = "cheese"
