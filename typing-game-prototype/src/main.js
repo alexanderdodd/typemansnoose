669ce72dff6bd32c20999ba4c72cdffcfa3c88b4
Alexander Dodd <45556671+alexanderdodd@users.noreply.github.com>
2024-11-30 17:00:35 +0000

    setup array of delims + available letters

--- typing-game-prototype/src/main.js
+++ typing-game-prototype/src/main.js
@@ -1,5 +1,36 @@
 import "./style.css";
 
+const LettersToParse = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+
+const Delimiters = [" "];
+
 document.getElementById("textInput").addEventListener("keyup", (val) => {
   document.getElementById("textOutput").innerHTML =
     document.getElementById("textOutput").innerHTML + val.key;
