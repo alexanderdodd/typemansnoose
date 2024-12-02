e5f02e54b52dc89d5fe345a0a5fcc85b838798d7
Alexander Dodd <45556671+alexanderdodd@users.noreply.github.com>
2024-12-02 09:54:12 +0000

    clear input after word entry

--- typing-game-prototype/src/main.js
+++ typing-game-prototype/src/main.js
@@ -1,7 +1,7 @@
 import "./style.css";
 import { parseWord } from "./parser";
 
-const wordsToDisplay =['cheese', 'is'];
+const wordsToDisplay = ["cheese", "is"];
 
 export function initialize() {
   document.getElementById("textInput").addEventListener("keydown", (event) => {
@@ -10,20 +10,23 @@ export function initialize() {
     if (val !== null) {
       document.getElementById("textOutput").innerHTML =
         document.getElementById("textOutput").innerHTML + " " + val;
+      if (val === wordsToDisplay[0]) {
+        document.getElementById(`word-${0}`).classList.add("success");
+      }
+      clearInput();
     }
-    if(val === wordsToDisplay[0]){
-      document.getElementById("wordsDisplay").classList.add("success");
-    }
-
   });
-let htmlWordsToDisplay = "";
+  let htmlWordsToDisplay = "";
 
-for(let i = 0; i < wordsToDisplay.length; i++){
-  htmlWordsToDisplay+=`<span id='word-${i}'>${wordsToDisplay[i]}</span> `;
-}
+  for (let i = 0; i < wordsToDisplay.length; i++) {
+    htmlWordsToDisplay += `<span id='word-${i}'>${wordsToDisplay[i]}</span> `;
+  }
 
   document.getElementById("wordsDisplay").innerHTML = htmlWordsToDisplay;
 }
 
+function clearInput() {
+  document.getElementById("textInput").value = "";
+}
 
 initialize();
