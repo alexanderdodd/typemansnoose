fbe4d3293bd9c98ba430a701bce3b0d16b0c9529
arran.toomer <arran.toomer@costcutter.com>
2025-02-13 20:30:30 +0000

    not sure where we are

--- typing-game-prototype/src/main.js
+++ typing-game-prototype/src/main.js
@@ -1,22 +1,37 @@
 import "./style.css";
 import { parseWord } from "./parser";
 
-const wordsToDisplay =['cheese', 'is'];
+const wordsToDisplay =['cheese', 'is', 'a','kind','of','meat'];
+const lettersToDisplay = [
+    ['c','h','e','e','s','e'],
+    ['i', 's']
+]
 
+
+
+
+let count = 0;
 export function initialize() {
   document.getElementById("textInput").addEventListener("keydown", (event) => {
-    const val = parseWord(event.code);
+    let val = parseWord(event.code);
 
     if (val !== null) {
       document.getElementById("textOutput").innerHTML =
         document.getElementById("textOutput").innerHTML + " " + val;
     }
-    if(val === wordsToDisplay[0]){
-      document.getElementById("wordsDisplay").classList.add("success");
+    if(val === wordsToDisplay[count]) {
+      document.getElementById("word-" + count).classList.add("success");
+      count++;
+     val = null;
+    }
+    if(val !== wordsToDisplay[count] && val != null){
+      document.getElementById("word-"+(count)).classList.add("failure");
+      count++;
     }
 
   });
 let htmlWordsToDisplay = "";
+let htmlLettersToDisplay = [];
 
 for(let i = 0; i < wordsToDisplay.length; i++){
   htmlWordsToDisplay+=`<span id='word-${i}'>${wordsToDisplay[i]}</span> `;
