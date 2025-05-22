0c39f9d508224688801433b378f750f860e79033
Arran Toomer <azztoomer@gmail.com>
2025-05-22 19:50:07 +0100

    does visual studio do what I ask and send my code?

--- react-typing-game-prototype/src/Container.tsx
+++ react-typing-game-prototype/src/Container.tsx
@@ -7,17 +7,22 @@ import TypedWords from "./TypedWords";
 import WordsPerMinute from "./WordsPerMinute";
 import WordsToType from "./WordsToType";
 
+
+
+
+
+
+
+
 function Container() {
-  const [wordsToTypeState, setWordsToTypeState] = useState<string>(
-    "too have too is will we shall him"
-  );
+  const [wordsToTypeState, setWordsToTypeState] = useState<string>("");
   const [totalTypedWords, setTotalTypedWords] = useState<string[]>([]);
   const [correctlyTypedWords, setCorrectlyTypedWords] = useState<number>(0);
   return (
     <>
       <div>
-        <div>
-          <WordsToType wordsToType={wordsToTypeState}/>
+      <div>
+          <WordsToType wordsToType={wordsToTypeState} setSentence={setSentence}/>
         </div>
         <div>
           <TypedWords nextWord={onNextWord}/>
@@ -40,7 +45,9 @@ function Container() {
     setWordsToTypeState("new sentence");
   }
 
-
+ function setSentence  (sentence: string){
+setWordsToTypeState(sentence);
+}
   function onNextWord(word: string) {
     console.log(word);
     setTotalTypedWords([...totalTypedWords, word])
